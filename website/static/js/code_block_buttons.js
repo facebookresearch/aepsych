e078e8eaee7842be1c49ff36b6f2efe8b8978666
Eric Cortez <ecortez@fb.com>
2022-06-10 11:08:34 -0700

    Added MIT LICENSE and cleanup
    Summary:
    - Added MIT LICENSE "Built with Docusaurus." to the footer
    - cleanup: removed unused auto-generated files/SVGs
    - updated SVG in code_block_buttons
    
    Differential Revision: D37075375
    
    fbshipit-source-id: 7faf0100fcd6cc88ff56151624a09c0e5ab60f18

--- website/static/js/code_block_buttons.js
+++ website/static/js/code_block_buttons.js
@@ -1,16 +1,14 @@
-// Turn off ESLint for this file because it's sent down to users as-is.
-/* eslint-disable */
+/* Home page "Copy" code block button */
 window.addEventListener('load', function() {
-  function button(label, ariaLabel, icon, className) {
+  function button(success, svgCopy, className) {
     const btn = document.createElement('button');
-    btn.classList.add('btnIcon', className);
+    btn.classList.add('svgIcon', className);
     btn.setAttribute('type', 'button');
-    btn.setAttribute('aria-label', ariaLabel);
     btn.innerHTML =
       '<div class="btnIcon__body">' +
-      icon +
-      '<strong class="btnIcon__label">' +
-      label +
+      svgCopy +
+      '<strong class="svgIcon__label">' +
+      success +
       '</strong>' +
       '</div>';
     return btn;
@@ -22,12 +20,14 @@ window.addEventListener('load', function() {
     });
   }
 
-  const copyIcon =
-    '<svg width="12" height="12" viewBox="340 364 14 15" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M342 375.974h4v.998h-4v-.998zm5-5.987h-5v.998h5v-.998zm2 2.994v-1.995l-3 2.993 3 2.994v-1.996h5v-1.995h-5zm-4.5-.997H342v.998h2.5v-.997zm-2.5 2.993h2.5v-.998H342v.998zm9 .998h1v1.996c-.016.28-.11.514-.297.702-.187.187-.422.28-.703.296h-10c-.547 0-1-.452-1-.998v-10.976c0-.546.453-.998 1-.998h3c0-1.107.89-1.996 2-1.996 1.11 0 2 .89 2 1.996h3c.547 0 1 .452 1 .998v4.99h-1v-2.995h-10v8.98h10v-1.996zm-9-7.983h8c0-.544-.453-.996-1-.996h-1c-.547 0-1-.453-1-.998 0-.546-.453-.998-1-.998-.547 0-1 .452-1 .998 0 .545-.453.998-1 .998h-1c-.547 0-1 .452-1 .997z" fill-rule="evenodd"/></svg>';
+ const svgCopy =
+  '<svg id="copy" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true"><path fill-rule="evenodd" d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 010 1.5h-1.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-1.5a.75.75 0 011.5 0v1.5A1.75 1.75 0 019.25 16h-7.5A1.75 1.75 0 010 14.25v-7.5z"></path><path fill-rule="evenodd" d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0114.25 11h-7.5A1.75 1.75 0 015 9.25v-7.5zm1.75-.25a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25h-7.5z"></path></svg>';
+const svgCheck =
+  '<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true"><path fill-rule="evenodd" fill="rgb(63, 185, 80)" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>';
 
   addButtons(
     '.hljs',
-    button('Copy', 'Copy code to clipboard', copyIcon, 'btnClipboard'),
+    button('', svgCopy, 'btnClipboard'),
   );
 
   const clipboard = new ClipboardJS('.btnClipboard', {
@@ -38,10 +38,11 @@ window.addEventListener('load', function() {
 
   clipboard.on('success', function(event) {
     event.clearSelection();
-    const textEl = event.trigger.querySelector('.btnIcon__label');
-    textEl.textContent = 'Copied';
+    const svgSuccess = event.trigger.querySelector('.svgIcon__label');
+
+    svgSuccess.innerHTML = ' <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true"><path fill-rule="evenodd" fill="rgb(63, 185, 80)" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>';
     setTimeout(function() {
-      textEl.textContent = 'Copy';
-    }, 2000);
+      svgSuccess.innerHTML = '';
+    }, 500);
   });
 });
