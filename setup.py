3bd6dc3fd93351d36e9895e09df4b3a8bb006e05
Craig Sanders <craigsanders@fb.com>
2022-06-16 12:14:04 -0700

    make installation and starting the server easier
    Summary: These changes will allow aepsych to be hosted on pypi and installed from pip; also allows a server to be started with a simple command
    
    Differential Revision: D37176812
    
    fbshipit-source-id: bafe19a1738d6f04d6c13766c7f9b7b83b248b70

--- setup.py
+++ setup.py
@@ -5,18 +5,57 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from os import path
+import os
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "Readme.md"), encoding="utf-8") as f:
-    long_description = f.read()
+root_dir = os.path.dirname(__file__)
+
+REQUIRES = [
+    "matplotlib",
+    "torch",
+    "pyzmq==19.0.2",
+    "scipy",
+    "sklearn",
+    "gpytorch>=1.4",
+    "botorch>=0.6.1",
+    "SQLAlchemy",
+    "dill",
+    "pandas",
+    "tqdm",
+    "pathos",
+]
+
+DEV_REQUIRES = [
+    "coverage",
+    "flake8",
+    "black",
+    "numpy>=1.20, " "sqlalchemy-stubs",  # for mypy stubs
+    "mypy",
+    "parameterized",
+]
+
+with open(os.path.join(root_dir, "README.md"), "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="aepsych",
-    version="0.1",
+    version="0.1.0",
+    python_requires=">=3.8",
     packages=find_packages(),
+    description="Adaptive experimetation for psychophysics",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    install_requires=REQUIRES,
+    entry_points={
+        "console_scripts": [
+            "aepsych_server = aepsych.server.server:main",
+        ],
+    },
+)
+
+extras_require = (
+    {
+        "dev": DEV_REQUIRES,
+    },
 )
