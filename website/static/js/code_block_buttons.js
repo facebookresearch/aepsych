a72cd2ea430e33a773b5d9628017ae8b64203a40
Eric Cortez <ecortez@fb.com>
2022-06-17 14:02:18 -0700

    Added Copyright header
    Summary:
    - Added "# Copyright (c) Meta Platforms, Inc. and affiliates." to resolve automated check for T123638805.
    - Also removed unused dockerfiles.
    - files that had other copyrights such as alabaseter.css and sphinx files were added to the Copyright Header Exemptions as they include there own copyright in the file.
    
    Differential Revision: D37218731
    
    fbshipit-source-id: e49387eb2a8359b63175359bafad62633eeab0e4

--- website/static/js/code_block_buttons.js
+++ website/static/js/code_block_buttons.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ For more information See https://docusaurus.io/
+ github repo: https://gist.github.com/yangshun/55db997ed0f8f4e6527571fc3bee4675
+ */
+
 /* Home page "Copy" code block button */
 window.addEventListener('load', function() {
   function button(success, svgCopy, className) {
