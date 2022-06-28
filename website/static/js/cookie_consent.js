5dee82f9418b64dd5555af5f634e07741293a3ef
Eric Cortez <ecortez@fb.com>
2022-06-28 08:53:52 -0700

    Added Missing copyright header
    Summary:
    - added copy right header to cookie_consent event listener
    - added the tutorials to the copyright header exemption as the header is not valid JSON and causes an error when building website.
    
    Differential Revision: D37489815
    
    fbshipit-source-id: a4856fe33f00101f44401fdc7ceccece5ad54a09

--- website/static/js/cookie_consent.js
+++ website/static/js/cookie_consent.js
@@ -1,3 +1,5 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved
+
 window.addEventListener('load', function() {
     const cookieConsentContainer = document.querySelector(".cookie-container");
     const cookieConsentBtn = document.querySelector(".cookie-btn");
