37c9942ca5fa2c95f2be96317bf089a1b0c77716
Eric-Corte <ecrcortez@gmail.com>
2022-08-24 12:44:12 -0700

    Deploy website
    Deploy website version based on a455151640110e0e083870137fa48738a58e4cfb

--- js/cookie_consent.js
+++ js/cookie_consent.js
@@ -3,6 +3,12 @@
 window.addEventListener('load', function() {
     const cookieConsentContainer = document.querySelector(".cookie-container");
     const cookieConsentBtn = document.querySelector(".cookie-btn");
+    const windowUrl = window.location.href
+    
+    if (windowUrl.includes("staticdocs")) {
+        cookieConsentContainer.classList.remove("active");
+        localStorage.setItem("cookieConsentDisplayed", true)
+    }
 
     cookieConsentBtn.addEventListener("click", () => {
         cookieConsentContainer.classList.remove("active");
