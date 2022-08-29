d1b6931bf9733e926d3a8edd6382801ba44d0bf0
Eric Cortez <ecortez@fb.com>
2022-08-29 09:20:37 -0700

    Added Copyright Header (T130533028)
    Summary: Added header to resolve Open Source Automated Checkup T130533028.
    
    Differential Revision: D39101940
    
    fbshipit-source-id: 543c405909d33aa3048745e4ec2811c39c1ec2ea

--- website/static/js/documentation_options.js
+++ website/static/js/documentation_options.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ For more information See https://docusaurus.io/
+ github repo: https://gist.github.com/yangshun/55db997ed0f8f4e6527571fc3bee4675
+ */
+
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '',
