6770b1016fe16276d1a4e489219ed1e86fb5b663
Ben Letham <bletham@fb.com>
2022-08-31 08:30:50 -0700

    Add a projection-based monotonic GP
    Summary:
    This adds a projection based monotonic GP, as described in the docstring of the model.
    
    I expect this to have much better scaling wrt number of features than the current monotonic rejection GP. On the high-dim discrim, it is doing something reasonable.
    
    Reviewed By: mshvartsman
    
    Differential Revision: D38745439
    
    fbshipit-source-id: 2f6367c00c80e91158b83045228b616fb3938a00

---
+++ aepsych/models/utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+from __future__ import annotations
+import torch
+
+
+def compute_p_quantile(f_mean: torch.Tensor, f_std: torch.Tensor, alpha: float) -> torch.Tensor:
+    """Compute quantile of p in probit model
+
+    For f ~ N(mu_f, sigma_f^2) and p = Phi(f), computes the alpha quantile of p
+    using the formula
+
+    x = Phi(mu_f + Phi^-1(alpha) * sigma_f),
+
+    which solves for x such that P(p <= x) = alpha.
+
+    A 95% CI for p can be computed as
+    p_l = compute_p_quantile(f_mean, f_std, 0.025)
+    p_u = compute_p_quantile(f_mean, f_std, 0.975)
+    """
+    norm = torch.distributions.Normal(0, 1)
+    alpha = torch.tensor(alpha, dtype=f_mean.dtype)
+    return norm.cdf(f_mean + norm.icdf(alpha) * f_std)
---
+++ tests/models/test_monotonic_projection_gp.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+import unittest
+
+import torch
+
+# run on single threads to keep us from deadlocking weirdly in CI
+if "CI" in os.environ or "SANDCASTLE" in os.environ:
+    torch.set_num_threads(1)
+
+import numpy as np
+from aepsych.models.monotonic_projection_gp import MonotonicProjectionGP
+from sklearn.datasets import make_classification
+
+
+class MonotonicProjectionGPtest(unittest.TestCase):
+
+    def setUp(self):
+        np.random.seed(1)
+        torch.manual_seed(1)
+        X, y = make_classification(
+            n_samples=25,
+            n_features=3,
+            n_redundant=0,
+            n_informative=3,
+            random_state=1,
+            n_clusters_per_class=1,
+        )
+        self.X, self.y = torch.Tensor(X), torch.Tensor(y)
+
+    def test_posterior(self):
+        X, y = self.X, self.y
+        model = MonotonicProjectionGP(
+            lb=torch.Tensor([-4, -4, -4]),
+            ub=torch.Tensor([4, 4, 4]),
+            inducing_size=10,
+            monotonic_dims=[0, 1],
+        )
+        model.fit(X, y)
+
+        # Check that it is monotonic in both dims
+        for i in range(2):
+            Xtest = torch.zeros(3, 3)
+            Xtest[:, i] = torch.tensor([-1, 0, 1])
+            post = model.posterior(Xtest)
+            mu = post.mean.squeeze()
+            self.assertTrue(
+                torch.equal(
+                    torch.tensor([0, 1, 2], dtype=torch.long),
+                    torch.argsort(mu),
+                )
+            )
+
+        # Check that min_f_val is respected
+        model = MonotonicProjectionGP(
+            lb=torch.Tensor([-4]),
+            ub=torch.Tensor([4]),
+            inducing_size=10,
+            monotonic_dims=[0],
+            min_f_val=5.0,
+        )
+        model.fit(X, y)
+        post = model.posterior(Xtest)
+        mu = post.mean.squeeze()
+        self.assertTrue(mu.min().item() >= 4.9)
+        # And in samples
+        samps = model.sample(Xtest, num_samples=10)
+        self.assertTrue(samps.min().item() >= 4.9)
+
+if __name__ == "__main__":
+    unittest.main()
