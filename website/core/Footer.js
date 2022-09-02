309ea51e6dd009aabd31cc6d073b048ac5a3bd8e
Eric Cortez <ecortez@fb.com>
2022-09-02 08:08:31 -0700

    Removed MIT License from root directory. (#140)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/aepsych/pull/140
    
    - Removed the MIT License from the root License directory and added it to the files that need it.
    
    Differential Revision: D39142745
    
    fbshipit-source-id: 025dd8d7ba533a2ea7f114b417d63d3f37651046

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -2,8 +2,9 @@
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */const PropTypes = require('prop-types');
+ * LICENSE file in the scripts directory.
+*/
+const PropTypes = require('prop-types');
 const React = require('react');
 
 
