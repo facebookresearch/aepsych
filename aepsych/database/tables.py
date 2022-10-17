976500aa68213c753adf044015a3875c31ee4c84
GabrielMissael <missaelgabo@gmail.com>
2022-10-17 08:44:10 -0500

    Add new table for multi-outcome support

--- aepsych/database/tables.py
+++ aepsych/database/tables.py
@@ -330,22 +330,20 @@ class DbRawTable(Base):
 
     unique_id = Column(Integer, primary_key=True, autoincrement=True)
     timestamp = Column(DateTime)
-    outcome = Column(Integer)
     model_data = Column(Boolean)
 
     master_table_id = Column(Integer, ForeignKey("master.unique_id"))
     parent = relationship("DBMasterTable", back_populates="children_raw")
     children_param = relationship("DbParamTable", back_populates="parent")
+    children_outcome = relationship("DbOutcomeTable", back_populates="parent")
 
     @classmethod
     def from_sqlite(cls, row):
         this = DbRawTable()
         this.unique_id = row["unique_id"]
         this.timestamp = row["timestamp"]
-        this.outcome = row["outcome"]
         this.model_data = row["model_data"]
         this.master_table_id = row["master_table_id"]
-        this.iteration_id = row["iteration_id"]
 
         return this
 
@@ -393,6 +391,40 @@ class DbParamTable(Base):
     @staticmethod
     def update(engine):
         logger.info("DbParamTable : update called")
+
+    @staticmethod
+    def requires_update(engine):
+        return False
+
+class DbOutcomeTable(Base):
+    __tablename__ = "outcome_data"
+
+    unique_id = Column(Integer, primary_key=True, autoincrement=True)
+    outcome_name = Column(String(50))
+    outcome_value = Column(Float)
+
+    iteration_id = Column(Integer, ForeignKey("raw_data.unique_id"))
+    parent = relationship("DbRawTable", back_populates="children_outcome")
+
+    @classmethod
+    def from_sqlite(cls, row):
+        this = DbOutcomeTable()
+        this.unique_id = row["unique_id"]
+        this.outcome_name = row["outcome_name"]
+        this.outcome_value = row["outcome_value"]
+        this.iteration_id = row["iteration_id"]
+
+        return this
+
+    def __repr__(self):
+        return (
+            f"<DbOutcomeTable(unique_id={self.unique_id})"
+            f", iteration_id={self.iteration_id}>"
+        )
+
+    @staticmethod
+    def update(engine):
+        logger.info("DbOutcomeTable : update called")
 
     @staticmethod
     def requires_update(engine):
