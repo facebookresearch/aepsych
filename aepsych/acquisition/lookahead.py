facebookresearch/aepsych

commit ef645d8ab1c5c8a4ce9f884c7592cdd93215ad99

Tidy up extra_acqf_args (#146)
Summary:
It's still a mess, but at least now we're not hardcoding acqf args, which resulted in mystery behavior and having to have a hardcoded grab bag of random default args in an unexpected place.

Pull Request resolved: https://github.com/facebookresearch/aepsych/pull/146

Reviewed By: crasanders

Differential Revision: D38292907

fbshipit-source-id: d8e35ada64929fcfb41811f3e38c8414552cb55e

aepsych/acquisition/lookahead.py
@@ -33,7 +33,7 @@ def Hb(p: Tensor):
 
     Returns: Binary entropy for each probability.
     """
-    epsilon = np.finfo(float).eps
+    epsilon = torch.tensor(np.finfo(float).eps)
     p = torch.clamp(p, min=epsilon, max=1 - epsilon)
     return -torch.nan_to_num(p * torch.log2(p) + (1 - p) * torch.log2(1 - p))
 

aepsych/acquisition/lse.py
@@ -24,8 +24,8 @@ class MCLevelSetEstimation(MCAcquisitionFunction):
     def __init__(
         self,
         model: Model,
-        target: Union[float, Tensor],
-        beta: Union[float, Tensor],
+        target: Union[float, Tensor] = 0.75,
+        beta: Union[float, Tensor] = 3.84,
         objective: Optional[MCAcquisitionObjective] = None,
         sampler: Optional[MCSampler] = None,
     ) -> None:

aepsych/config.py
@@ -10,7 +10,7 @@ import configparser
 import json
 import warnings
 from types import ModuleType
-from typing import Any, Dict, List, Mapping, Optional, Sequence, TypeVar
+from typing import Any, ClassVar, Dict, List, Mapping, Optional, Sequence, TypeVar
 
 import botorch
 import gpytorch
@@ -25,7 +25,7 @@ _T = TypeVar("_T")
 class Config(configparser.ConfigParser):
 
     # names in these packages can be referred to by string name
-    registered_names: Dict[str, object] = {}
+    registered_names: ClassVar[Dict[str, object]] = {}
 
     def __init__(
         self,
@@ -222,13 +222,20 @@ class Config(configparser.ConfigParser):
             )
         cls.registered_names.update({obj.__name__: obj})
 
+    def get_section(self, section):
+        sec = {}
+        for setting in self[section]:
+            if section != "common" and setting in self["common"]:
+                continue
+            sec[setting] = self[section][setting]
+        return sec
+
     def __str__(self):
         _str = ""
         for section in self:
+            sec = self.get_section(section)
             _str += f"[{section}]\n"
-            for setting in self[section]:
-                if section != "common" and setting in self["common"]:
-                    continue
+            for setting in sec:
                 _str += f"{setting} = {self[section][setting]}\n"
         return _str
 
@@ -352,3 +359,4 @@ class Config(configparser.ConfigParser):
 
 Config.register_module(gpytorch.kernels)
 Config.register_module(botorch.acquisition)
+Config.registered_names["None"] = None

aepsych/generators/base.py
@@ -52,23 +52,31 @@ class AEPsychGenerator(abc.ABC, Generic[AEPsychModelType]):
     def _get_acqf_options(cls, acqf: AcquisitionFunction, config: Config):
         if acqf is not None:
             acqf_name = acqf.__name__
-            default_extra_acqf_args = {
-                "beta": 3.84,
-                "target": 0.75,
-                "objective": None,
-                "query_set_size": 512,
-                "posterior_transform": None,
-            }
-            extra_acqf_args = {
-                k: config.getobj(
-                    acqf_name, k, fallback_type=float, fallback=v, warn=False
-                )
-                for k, v in default_extra_acqf_args.items()
-            }
-            acqf_args_expected = signature(acqf).parameters.keys()
-            extra_acqf_args = {
-                k: v for k, v in extra_acqf_args.items() if k in acqf_args_expected
-            }
+
+            # model is not an extra arg, it's a default arg
+            acqf_args_expected = [
+                i for i in list(signature(acqf).parameters.keys()) if i != "model"
+            ]
+
+            # this is still very ugly
+            extra_acqf_args = {}
+            if acqf_name in config: 
+                full_section = config[acqf_name]
+                for k in acqf_args_expected:
+                    # if this thing is configured
+                    if k in full_section.keys():
+                        # if it's an object make it an object
+                        if full_section[k] in config.registered_names.keys():
+                            extra_acqf_args[k] = config.getobj(acqf_name, k)
+                        else:
+                            # otherwise try a float
+                            try:
+                                extra_acqf_args[k] = config.getfloat(acqf_name, k)
+                            # finally just return a string
+                            except ValueError:
+                                extra_acqf_args[k] = config.get(acqf_name, k)
+
+            # next, do more processing
             for k, v in extra_acqf_args.items():
                 if hasattr(v, "from_config"):  # configure if needed
                     assert isinstance(v, AcqArgProtocol)  # make mypy happy

tests/test_config.py
@@ -57,6 +57,7 @@ class ConfigTestCase(unittest.TestCase):
         keep_most_recent = 10
 
         [MCLevelSetEstimation]
+        target = 0.75
         beta = 3.84
         objective = ProbitObjective
 
@@ -71,6 +72,14 @@ class ConfigTestCase(unittest.TestCase):
         config = Config()
         config.update(config_str=config_str)
 
+        self.assertTrue(
+            config.get_section("MCLevelSetEstimation")
+            == {"beta": "3.84", "objective": "ProbitObjective", "target":"0.75"}
+        )
+        self.assertTrue(
+            config.get_section("OptimizeAcqfGenerator")
+            == {"restarts": "10", "samps": "1000"}
+        )
         strat = SequentialStrategy.from_config(config)
 
         self.assertTrue(isinstance(strat.strat_list[0].generator, SobolGenerator))
@@ -84,6 +93,7 @@ class ConfigTestCase(unittest.TestCase):
             set(strat.strat_list[1].generator.acqf_kwargs.keys())
             == {"beta", "target", "objective"}
         )
+
         self.assertTrue(strat.strat_list[1].generator.acqf_kwargs["target"] == 0.75)
         self.assertTrue(strat.strat_list[1].generator.acqf_kwargs["beta"] == 3.84)
         self.assertTrue(
@@ -161,6 +171,7 @@ class ConfigTestCase(unittest.TestCase):
         self.assertTrue(torch.all(strat.strat_list[1].model.ub == torch.Tensor([1, 1])))
 
     def test_nonmonotonic_optimization_config_file(self):
+        
         config_file = "../configs/nonmonotonic_optimization_example.ini"
         config_file = os.path.join(os.path.dirname(__file__), config_file)
 
@@ -177,7 +188,7 @@ class ConfigTestCase(unittest.TestCase):
         self.assertTrue(strat.strat_list[1].generator.acqf is qNoisyExpectedImprovement)
         self.assertTrue(
             set(strat.strat_list[1].generator.acqf_kwargs.keys())
-            == {"objective", "posterior_transform"}
+            == {"objective"}
         )
         self.assertTrue(
             isinstance(
@@ -540,7 +551,7 @@ class ConfigTestCase(unittest.TestCase):
         self.assertTrue(strat.strat_list[1].generator.acqf is qNoisyExpectedImprovement)
         self.assertTrue(
             set(strat.strat_list[1].generator.acqf_kwargs.keys())
-            == {"objective", "posterior_transform"}
+            == {"objective"}
         )
         self.assertTrue(
             isinstance(

commit 2dbeab2244a327ff4b530db40a64a0eaeb3f4a65

Fix CSF experiment issue caused by #146 (#167)
Summary:
The acqf args fix in https://github.com/facebookresearch/aepsych/issues/146 broke the example CSF experiment. This fixes.

Pull Request resolved: https://github.com/facebookresearch/aepsych/pull/167

Test Plan: Ran ~10 trials of the experiment on myself.

Differential Revision: D40371247

fbshipit-source-id: eacf2552cc47a050cfd0613d4bec832b8805ee82

aepsych/acquisition/lookahead.py
@@ -5,7 +5,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Optional, Tuple
+from typing import cast, Optional, Tuple
 
 import numpy as np
 import torch
@@ -131,10 +131,10 @@ class GlobalLookaheadAcquisitionFunction(LookaheadAcquisitionFunction):
     def __init__(
         self,
         model: GPyTorchModel,
-        lookahead_type="levelset",
+        lookahead_type: str = "levelset",
         target: Optional[float] = None,
         posterior_transform: Optional[PosteriorTransform] = None,
-        query_set_size: Optional[int] = None,
+        query_set_size: Optional[int] = 256,
         Xq: Optional[Tensor] = None,
     ) -> None:
         """
@@ -156,6 +156,11 @@ class GlobalLookaheadAcquisitionFunction(LookaheadAcquisitionFunction):
                 + "first dim of Xq should be query_set_size, got {Xq.shape[0]} != {query_set_size}"
             )
 
+        # cast to an int in case we got a float from Config, which
+        # would raise on make_scaled_sobol
+        query_set_size = cast(int, query_set_size)  # make mypy happy
+        assert int(query_set_size) == query_set_size  # make sure casting is safe
+        query_set_size = int(query_set_size)  # cast
         Xq = (
             Xq
             if Xq is not None
@@ -209,7 +214,7 @@ class ApproxGlobalSUR(GlobalSUR):
         model: GPyTorchModel,
         lookahead_type="levelset",
         target: Optional[float] = None,
-        query_set_size: Optional[int] = None,
+        query_set_size: Optional[int] = 256,
         Xq: Optional[Tensor] = None,
     ) -> None:
         assert (

examples/contrast_discrimination_psychopy/aepsych_config.ini
@@ -10,7 +10,6 @@ parnames = [pedestal, contrast, temporal_frequency, spatial_frequency, size, ecc
 lb = [-1.5, -1.5, 0, 0.5, 1, 0]
 ub = [0, 0, 20, 7, 10, 10]
 outcome_type = single_probit
-target = 0.75
 strategy_names = [init_strat, opt_strat] # The strategies that will be used, corresponding to the named sections below
 acqf = GlobalMI
 
@@ -48,3 +47,6 @@ restarts = 2
 # number of samples for quasi-random initialization of the acquisition function optimizer
 samps = 100
 max_gen_time=0.5
+
+[GlobalMI]
+target = 0.75