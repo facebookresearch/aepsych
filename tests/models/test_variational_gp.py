dd40b6d8254e9c062d2944682888a2c40e8525f5
kamda-cyrial <42605286+kamda-cyrial@users.noreply.github.com>
2023-03-10 13:52:09 -0500

    sample test for axordinal model fitting

--- tests/models/test_variational_gp.py
+++ tests/models/test_variational_gp.py
@@ -12,10 +12,12 @@ import numpy.testing as npt
 import torch
 from botorch.fit import fit_gpytorch_mll
 from gpytorch.likelihoods import BernoulliLikelihood
+from aepsych.likelihoods.ordinal import OrdinalLikelihood
 from gpytorch.mlls import VariationalELBO
 from sklearn.datasets import make_classification
 
 from aepsych.models import BinaryClassificationGP
+from aepsych.models.variational_gp import OrdinalGP
 
 
 class BinaryClassificationGPTestCase(unittest.TestCase):
@@ -59,6 +61,71 @@ class BinaryClassificationGPTestCase(unittest.TestCase):
         pred = (pm > 0).numpy()
         npt.assert_allclose(pred.reshape(-1, 1), y)
         npt.assert_array_less(1, pv)
+
+
+class AxOrdinalGPTestCase(unittest.TestCase):
+    # @classmethod
+    # def setup(cls):
+    def test_ordinal_classification(self):
+        np.random.seed(1)
+        torch.manual_seed(1)
+        n_levels = 5
+        X, y = make_classification(
+            n_samples=20,
+            n_features=5,
+            n_classes=n_levels,
+            n_informative=3,
+            n_clusters_per_class=1,
+        )
+        self.X, self.y = torch.Tensor(X), torch.Tensor(y).reshape(-1, 1)
+
+        model = OrdinalGP(
+            train_X=self.X,
+            train_Y=self.y,
+            likelihood=OrdinalLikelihood(n_levels=n_levels),
+            inducing_points=2000,
+        )
+        probs = model.predict(self.X, probability_space=True)
+        pred = np.argmax(probs.detach().numpy(), axis=1).reshape(-1, 1)
+        print(
+            "predicted: ",
+            pred.reshape(-1),
+            "expected: ",
+            self.y.detach().numpy().reshape(-1),
+        )
+        mll = VariationalELBO(model.likelihood, model.model, len(self.y))
+        fit_gpytorch_mll(mll)
+
+        # pspace
+        probs = model.predict(self.X, probability_space=True)
+        pred = np.argmax(probs.detach().numpy(), axis=1).reshape(-1, 1)
+        print(
+            "predicted: ",
+            pred.reshape(-1),
+            "expected: ",
+            self.y.detach().numpy().reshape(-1),
+        )
+        clipped_pred = np.clip(pred, 0, n_levels)
+        npt.assert_allclose(clipped_pred, pred, atol=1, rtol=1)
+        npt.assert_allclose(pred, self.y, atol=1, rtol=1)
+
+        # fspace
+        pm, pv = model.predict(self.X, probability_space=False)
+        pred = np.floor(n_levels * pm).reshape(-1, 1)
+        pred_var = (n_levels * pv).reshape(-1, 1)
+        print(
+            "predicted: ",
+            pred.detach().numpy().reshape(-1),
+            "expected: ",
+            self.y.detach().numpy().reshape(-1),
+            sep=" ",
+        )
+        clipped_pred = np.clip(pred, 0, n_levels)
+        npt.assert_allclose(clipped_pred, pred, atol=3, rtol=n_levels)
+        npt.assert_allclose(pred, self.y, atol=3, rtol=n_levels)
+        npt.assert_allclose(
+            pred_var, np.ones_like(pred_var), atol=n_levels, rtol=n_levels
+        )
 
 
 if __name__ == "__main__":
