1c2794c2faddeac35aead6a953c200c562756b83
kamda-cyrial <42605286+kamda-cyrial@users.noreply.github.com>
2023-03-30 17:49:01 -0400

    getting to a model that can go through the client process, yet to test memorization and also I am getting a 'cache root is only supported for Gpytorch models warning'

--- aepsych/models/ax_pairwise_probit.py
+++ aepsych/models/ax_pairwise_probit.py
@@ -1,56 +1,35 @@
 from __future__ import annotations
-import os
-
 from botorch.models.pairwise_gp import PairwiseGP
 import torch
 from aepsych.config import Config
 from botorch.models.likelihoods.pairwise import (
     PairwiseProbitLikelihood,
-    PairwiseLikelihood,
 )
 from aepsych.models.base import AEPsychModel
-from sklearn.datasets import make_classification
-from aepsych.utils import get_dim, promote_0d, _process_bounds
+from aepsych.utils import promote_0d
 from scipy.stats import norm
 from aepsych.models.utils import select_inducing_points
-from aepsych.models.variational_gp import BinaryClassificationGP
-
-import time, gpytorch, numpy as np
-from typing import Any, Dict, Optional, Union, Type, Tuple
+from typing import Optional, Union, Type, Tuple
 from aepsych.factory import default_mean_covar_factory
 from aepsych.config import Config
 from aepsych.utils_logging import getLogger
-from botorch.fit import fit_gpytorch_mll
 from botorch.models import PairwiseGP, PairwiseLaplaceMarginalLogLikelihood
 from botorch.models.transforms.input import Normalize
-from botorch.models.model import Model
 from botorch.models.transforms.input import InputTransform
 from botorch.models.transforms.outcome import OutcomeTransform
 from gpytorch.kernels import Kernel
 from gpytorch.likelihoods.likelihood import Likelihood
-from gpytorch.mlls.marginal_log_likelihood import MarginalLogLikelihood
 from gpytorch.variational import (
     _VariationalDistribution,
     _VariationalStrategy,
-    CholeskyVariationalDistribution,
-    IndependentMultitaskVariationalStrategy,
     VariationalStrategy,
 )
-
 from botorch.models.utils.inducing_point_allocators import (
-    GreedyVarianceReduction,
     InducingPointAllocator,
 )
-
-from gpytorch.means import ConstantMean, Mean
-import dataclasses
-import time
-from typing import Dict, List, Optional
+from gpytorch.means import Mean
+from typing import Optional
 from aepsych.utils_logging import getLogger
-from ax.core.search_space import SearchSpaceDigest
-from ax.core.types import TCandidateMetadata
-from botorch.fit import fit_gpytorch_mll
-from botorch.utils.datasets import SupervisedDataset
 from torch import Tensor
 
 
@@ -94,10 +73,8 @@ class PairwiseGPModel(AEPsychModel, PairwiseGP):
             )  # type: ignore
             _, covar_module = default_mean_covar_factory(config)
 
-        datapoints, comparisons = self._pairs_to_comparisons(train_X, train_Y)
-        print("Datapoints: ", datapoints, " Comparisons: ", comparisons)
         super().__init__(
-            datapoints=datapoints,
+            datapoints=None,
             comparisons=None,
             covar_module=covar_module,
             jitter=1e-3,
@@ -107,44 +84,15 @@ class PairwiseGPModel(AEPsychModel, PairwiseGP):
         self.covar_module = covar_module
         self.botorch_model_class = None
         self.dim = dim  # The Pairwise constructor sets self.dim = None.
-        self.datapoints = datapoints
 
     def _pairs_to_comparisons(
         self, x: torch.tensor, y: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.LongTensor]:
-        """
-        Takes x, y structured as pairs and judgments and
-        returns pairs and comparisons as PairwiseGP requires
-        """
-        # This needs to take a unique over the feature dim by flattening
-        # over pairs but not instances/batches. This is actually tensor
-        # matricization over the feature dimension but awkward in numpy
-        unique_coords = torch.unique(
-            torch.transpose(x, 1, 0).reshape(self.dim, -1), dim=1
-        )                                    
-
-        def _get_index_of_equal_row(arr, x: torch.Tensor, dim=0):
-            # print("Inputed X: ", x.unsqueeze(-1))
-            # print("Inputed arr: ", arr)
-            torch_eq = torch.eq(arr, x).reshape(1, -1)
-            # print("Torch.eq: ", torch_eq)
-            # print("Torch.all: ", torch.all(torch_eq, dim=dim))
-            item = torch.all(torch_eq, dim=dim).nonzero().item()
-            # print("Item: ", item)
-            return item
-
         comparisons = []
-        for pair, judgement in zip(x, y):
-            # print("Pair: ", pair)
-            comparison = (
-                _get_index_of_equal_row(unique_coords[0, ...], pair[..., 0]),
-                _get_index_of_equal_row(unique_coords[1, ...], pair[..., 1]),
-            )
-            if judgement == 0:
-                comparisons.append(comparison)
-            else:
-                comparisons.append(comparison[::-1])
-        return unique_coords.T, torch.LongTensor(comparisons)
+        for i in range(x.shape[0]):
+            for j in range(i + 1, x.shape[0]):
+                comparisons.append((i, j) if y[i] > y[j] else (j, i))
+        return x, torch.LongTensor(comparisons)
 
     @classmethod
     def get_mll_class(cls):
@@ -167,50 +115,6 @@ class PairwiseGPModel(AEPsychModel, PairwiseGP):
             )
         else:
             return fmean, fvar
-
-    # def fit(
-    #     self,
-    #     datasets: List[SupervisedDataset],
-    #     metric_names: List[str],
-    #     search_space_digest: SearchSpaceDigest,
-    #     candidate_metadata: Optional[List[List[TCandidateMetadata]]] = None,
-    #     state_dict: Optional[Dict[str, Tensor]] = None,
-    #     refit: bool = True,
-    #     **kwargs,
-    # ) -> None:
-    #     print("Fitting PairwiseGPModel ..................")
-    #     self.train()
-    #     self._outcomes = metric_names
-    #     if state_dict:
-    #         self.model.load_state_dict(state_dict)
-
-    #     if state_dict is None or refit:
-    #         mll = self.get_mll_class()(self.likelihood, self)
-    #         optimizer_kwargs = {}
-    #         if self.max_fit_time is not None:
-    #             # figure out how long evaluating a single samp
-    #             starttime = time.time()
-
-    #             if isinstance(self, PairwiseGPModel):
-    #                 datapoints, comparisons = self._pairs_to_comparisons(
-    #                     datasets[0].X(), datasets[0].Y().squeeze()
-    #                 )
-    #                 self.set_train_data(datapoints, comparisons)
-    #                 _ = mll(self.model(datapoints), comparisons)
-    #             else:
-    #                 _ = mll(self.model(datasets[0].X()), datasets[0].Y().squeeze())
-    #             single_eval_time = time.time() - starttime
-    #             n_eval = int(self.max_fit_time / single_eval_time)
-    #             logger.info(f"fit maxfun is {n_eval}")
-    #             optimizer_kwargs["options"] = {"maxfun": n_eval}
-
-    #         logger.info("Starting fit...")
-    #         print(self.max_fit_time)
-    #         starttime = time.time()
-    #         fit_gpytorch_mll(
-    #             mll, optimizer_kwargs=optimizer_kwargs
-    #         )  # TODO: Support flexible optimizers
-    #         logger.info(f"Fit done, time={time.time()-starttime}")
 
     @classmethod
     def construct_inputs(cls, training_data, **kwargs):
@@ -260,7 +164,3 @@ class PairwiseGPModel(AEPsychModel, PairwiseGP):
             }
         )
         return options
-
-
-# a = PairwiseGPModel().train_inputs
-# b = BinaryClassificationGP().forward()
--- aepsych/models/surrogate.py
+++ aepsych/models/surrogate.py
@@ -32,42 +32,17 @@ class AEPsychSurrogate(Surrogate):
         optimizer_kwargs: Optional[Dict[str, Any]] = None,
         **kwargs,
     ) -> None:
-        # if isinstance(self.model_options["likelihood"], PairwiseProbitLikelihood):
-        #     print("Fitting PairwiseGPModel ..................")
-        #     self.train()
-        #     self._outcomes = metric_names
-        #     if state_dict:
-        #         self.model.load_state_dict(state_dict)
-
-        #     if state_dict is None or refit:
-        #         mll = self.get_mll_class()(self.likelihood, self)
-        #         optimizer_kwargs = {}
-        #         if self.max_fit_time is not None:
-        #             # figure out how long evaluating a single samp
-        #             starttime = time.time()
-        #             datapoints, comparisons = self._pairs_to_comparisons(
-        #                 datasets[0].X(), datasets[0].Y().squeeze()
-        #             )
-        #             self.set_train_data(datapoints, comparisons)
-        #             _ = mll(self.model(datapoints), comparisons)
-        #             single_eval_time = time.time() - starttime
-        #             n_eval = int(self.max_fit_time / single_eval_time)
-        #             logger.info(f"fit maxfun is {n_eval}")
-        #             optimizer_kwargs["options"] = {"maxfun": n_eval}
-
-        #         logger.info("Starting fit, with max_fit_time: ", end="")
-        #         print(self.max_fit_time, "...")
-        #         starttime = time.time()
-        #         fit_gpytorch_mll(
-        #             mll, optimizer_kwargs=optimizer_kwargs
-        #         )  # TODO: Support flexible optimizers
-        #         logger.info(f"Fit done, time={time.time()-starttime}")
-        # else:
         self.construct(
             datasets=datasets,
             metric_names=metric_names,
             **dataclasses.asdict(search_space_digest),
         )
+        if isinstance(self.model_options["likelihood"], PairwiseProbitLikelihood):
+            datapoints, comparisons = self.model._pairs_to_comparisons(
+                datasets[0].X(), datasets[0].Y().squeeze()
+            )
+            self.model.set_train_data(datapoints, comparisons)
+
         self._outcomes = metric_names
         if state_dict:
             self.model.load_state_dict(state_dict)
@@ -79,8 +54,14 @@ class AEPsychSurrogate(Surrogate):
             )
             if self.max_fit_time is not None:
                 # figure out how long evaluating a single samp
+                print("max_fit_time is not None .............")
                 starttime = time.time()
-                _ = mll(self.model(datasets[0].X()), datasets[0].Y().squeeze())
+                if isinstance(
+                    self.model_options["likelihood"], PairwiseProbitLikelihood
+                ):
+                    _ = mll(self.model(datapoints), comparisons)
+                else:
+                    _ = mll(self.model(datasets[0].X()), datasets[0].Y().squeeze())
                 single_eval_time = time.time() - starttime
                 n_eval = int(self.max_fit_time / single_eval_time)
                 logger.info(f"fit maxfun is {n_eval}")
