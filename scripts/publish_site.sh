18c23efaef627bb82d69e319cba8e5b6070b18a7
Eric-Corte <ecrcortez@gmail.com>
2023-05-25 17:58:37 -0700

    updated workflow

--- scripts/publish_site.sh
+++ scripts/publish_site.sh
@@ -35,10 +35,10 @@ echo "=========================================="
 echo "======== Cleanup for github ==============="
   cd .. || exit
   find . -type d -name __pycache__ -exec rm -r {} \+
-  cd website/ || exit
-  rm -rf node_modules/
-  rm -rf yarn.lock
-  cd ../
-  cd sphinx/ || exit
-  rm -rf build/ || exit
+  # cd website/ || exit
+  # rm -rf node_modules/
+  # rm -rf yarn.lock
+  # cd ../
+  # cd sphinx/ || exit
+  # rm -rf build/ || exit
 echo "================Completed======================"
