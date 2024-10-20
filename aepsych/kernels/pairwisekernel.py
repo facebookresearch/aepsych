3a31698bb91f215ac388f5354a06d2ef6065293c
Yousif Alsaffar <yalsaffar.ieu2020@student.ie.edu>
2024-10-20 19:08:44 +0300

    adding type hints to kernels files

--- aepsych/kernels/pairwisekernel.py
+++ aepsych/kernels/pairwisekernel.py
@@ -1,7 +1,9 @@
+from typing import Any, Optional
 import torch
 from gpytorch.kernels import Kernel
 from linear_operator import to_linear_operator
-
+from gpytorch.kernels import RBFKernel
+from pytorch.lazy import LazyTensor
 
 class PairwiseKernel(Kernel):
     """
@@ -13,13 +15,13 @@ class PairwiseKernel(Kernel):
 
     """
 
-    def __init__(self, latent_kernel, is_partial_obs=False, **kwargs):
+    def __init__(self, latent_kernel: Any, is_partial_obs: bool=False, **kwargs) -> None:
         super(PairwiseKernel, self).__init__(**kwargs)
 
         self.latent_kernel = latent_kernel
         self.is_partial_obs = is_partial_obs
 
-    def forward(self, x1, x2, diag=False, **params):
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor, diag: bool=False, **params) -> tuple[torch.Tensor, LazyTensor]:
         r"""
         TODO: make last_batch_dim work properly
 
@@ -40,11 +42,11 @@ class PairwiseKernel(Kernel):
                 * `diag`: `n` or `b x n`
         """
         if self.is_partial_obs:
-            d = x1.shape[-1] - 1
-            assert d == x2.shape[-1] - 1, "tensors not the same dimension"
-            assert d % 2 == 0, "dimension must be even"
+            d_ = x1.shape[-1] - 1 #avoiding assigning d int then tensor(for mypy)
+            assert d_ == x2.shape[-1] - 1, "tensors not the same dimension"
+            assert d_ % 2 == 0, "dimension must be even"
 
-            k = int(d / 2)
+            k = int(d_ / 2)
 
             # special handling for kernels that (also) do funky
             # things with the input dimension
@@ -57,12 +59,12 @@ class PairwiseKernel(Kernel):
             d = torch.cat((x2[..., k:-1], deriv_idx_2), dim=1)
 
         else:
-            d = x1.shape[-1]
+            d_ = x1.shape[-1]
 
-            assert d == x2.shape[-1], "tensors not the same dimension"
-            assert d % 2 == 0, "dimension must be even"
+            assert d_ == x2.shape[-1], "tensors not the same dimension"
+            assert d_ % 2 == 0, "dimension must be even"
 
-            k = int(d / 2)
+            k = int(d_ / 2)
 
             a = x1[..., :k]
             b = x1[..., k:]
