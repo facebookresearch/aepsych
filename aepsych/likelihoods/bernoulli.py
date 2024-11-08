654cd9cdeaff34595614ed2bd536d7eb4845eb5c
Yousif Alsaffar <yalsaffar.ieu2020@student.ie.edu>
2024-11-08 09:52:48 -0800

    Enhance Docstrings in `aepsych/likelihoods` (#426)
    Summary:
    One of several PRs addressing issue https://github.com/facebookresearch/aepsych/issues/366 to improve docstring coverage.
    
    Improves documentation in `aepsych/likelihoods` for better clarity and consistency.
    - Adds missing docstrings to functions and methods across likelihood modules.
    - Updates existing docstrings with refined type hints and a unified structure.
    
    Pull Request resolved: https://github.com/facebookresearch/aepsych/pull/426
    
    Reviewed By: crasanders
    
    Differential Revision: D65639791
    
    Pulled By: JasonKChow
    
    fbshipit-source-id: c984880e6285367b1ef0fb95f1ff45612ccc7d50

--- aepsych/likelihoods/bernoulli.py
+++ aepsych/likelihoods/bernoulli.py
@@ -18,15 +18,35 @@ class BernoulliObjectiveLikelihood(_OneDimensionalLikelihood):
     """
 
     def __init__(self, objective: Callable) -> None:
+        """Initialize BernoulliObjectiveLikelihood.
+        
+        Args:
+            objective (Callable): Objective function that maps function samples to probabilities."""
         super().__init__()
         self.objective = objective
 
     def forward(self, function_samples: torch.Tensor, **kwargs: Any) -> torch.distributions.Bernoulli:
+        """Forward pass for BernoulliObjectiveLikelihood.
+
+        Args:
+            function_samples (torch.Tensor): Function samples.
+
+        Returns:
+            torch.distributions.Bernoulli: Bernoulli distribution object.
+        """
         output_probs = self.objective(function_samples)
         return torch.distributions.Bernoulli(probs=output_probs)
 
     @classmethod
     def from_config(cls, config: Config) -> 'BernoulliObjectiveLikelihood':
+        """Create an instance from a configuration object.
+        
+        Args:
+            config (Config): Configuration object.
+            
+        Returns:
+            BernoulliObjectiveLikelihood: BernoulliObjectiveLikelihood instance.
+        """
         objective_cls = config.getobj(cls.__name__, "objective")
         objective = objective_cls.from_config(config)
         return cls(objective=objective)
