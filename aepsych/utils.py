91f88d453f23cd3f72bf3339523c50f545fdf748
Yousif Alsaffar <yalsaffar.ieu2020@student.ie.edu>
2024-12-02 21:10:20 +0300

    add a missing typehint and fix mypy errors

--- aepsych/utils.py
+++ aepsych/utils.py
@@ -40,17 +40,18 @@ def make_scaled_sobol(
     return grid
 
 
-def promote_0d(x: Union[torch.Tensor, np.ndarray]) -> Union[Iterable[np.ndarray], torch.Tensor]:
+def promote_0d(x: Union[torch.Tensor, np.ndarray, List]) -> torch.Tensor:
     """Promote a 0d tensor to a 1d tensor
     
     Args:
         x (Union[torch.Tensor, np.ndarray]): Input tensor
 
     Returns:
-        List[Union[torch.Tensor, np.ndarray]]: Promoted tensor.
+        torch.Tensor: Promoted tensor.
     """
     if not isinstance(x, Iterable):
-        return [x]
+        x = [x]
+    x = torch.tensor(x)
     return x
 
 
@@ -89,15 +90,15 @@ def dim_grid(
 
 
 def _process_bounds(
-    lb: Union[Iterable[np.ndarray], torch.Tensor],
-    ub: Union[Iterable[np.ndarray], torch.Tensor],
+    lb: Union[np.ndarray, torch.Tensor, List],
+    ub: Union[np.ndarray, torch.Tensor, List],
     dim: Optional[int],
 ) -> Tuple[torch.Tensor, torch.Tensor, int]:
     """Helper function for ensuring bounds are correct shape and type.
     
     Args:
-        lb (Union[torch.Tensor, np.ndarray, List[Union[torch.Tensor, np.ndarray]]]): Lower bounds.
-        ub (Union[torch.Tensor, np.ndarray, List[Union[torch.Tensor, np.ndarray]]]): Upper bounds.
+        lb (Union[np.ndarray, torch.Tensor, List]): Lower bounds.
+        ub (Union[np.ndarray, torch.Tensor, List]): Upper bounds.
         dim (int, optional): Dimension of the bounds.
         
     Returns:
