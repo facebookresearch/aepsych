4f5127ed21bdc5e6723b4f3e334a9f81679279e4
Jason Chow <jasonchow@meta.com>
2025-02-04 12:47:24 -0800

    Update all likelihoods to use ConfigurableMixin and auto init parsing (#610)
    Summary:
    
    All likelihoods with from_config now use configurable mixin. Uses automated init parsing where possible with additional support for bespoke behavior.
    
    Reviewed By: crasanders
    
    Differential Revision: D69087735

--- aepsych/likelihoods/bernoulli.py
+++ aepsych/likelihoods/bernoulli.py
@@ -4,14 +4,14 @@
 
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-from typing import Any, Callable
+from typing import Any, Callable, Dict, Optional
 
 import torch
-from aepsych.config import Config
+from aepsych.config import ConfigurableMixin
 from gpytorch.likelihoods import _OneDimensionalLikelihood
 
 
-class BernoulliObjectiveLikelihood(_OneDimensionalLikelihood):
+class BernoulliObjectiveLikelihood(_OneDimensionalLikelihood, ConfigurableMixin):
     """
     Bernoulli likelihood with a flexible link (objective) defined
     by a callable (which can be a botorch objective)
@@ -21,7 +21,8 @@ class BernoulliObjectiveLikelihood(_OneDimensionalLikelihood):
         """Initialize BernoulliObjectiveLikelihood.
 
         Args:
-            objective (Callable): Objective function that maps function samples to probabilities."""
+            objective (Callable): Objective function that maps function samples to probabilities.
+        """
         super().__init__()
         self.objective = objective
 
@@ -40,15 +41,23 @@ class BernoulliObjectiveLikelihood(_OneDimensionalLikelihood):
         return torch.distributions.Bernoulli(probs=output_probs)
 
     @classmethod
-    def from_config(cls, config: Config) -> "BernoulliObjectiveLikelihood":
-        """Create an instance from a configuration object.
+    def get_config_options(
+        cls,
+        config,
+        name: Optional[str] = None,
+        options: Optional[Dict[str, Any]] = None,
+    ) -> Dict[str, Any]:
+        """Find the config options for the likelihood.
 
         Args:
-            config (Config): Configuration object.
+            config (Config): Config to look for options in.
+            name (str, optional): Unused, kept for API conformity.
+            options (Dict[str, Any], optional): Existing options, any key in options
+                will be ignored from the config.
 
-        Returns:
-            BernoulliObjectiveLikelihood: BernoulliObjectiveLikelihood instance.
+        Return:
+            Dict[str, Any]: A dictionary of options to initialize the likelihood.
         """
-        objective_cls = config.getobj(cls.__name__, "objective")
-        objective = objective_cls.from_config(config)
-        return cls(objective=objective)
+        options = super().get_config_options(config, name, options)
+        options["objective"] = options["objective"].from_config(config)
+        return options
