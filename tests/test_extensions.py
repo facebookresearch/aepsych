15f8e78e2e1364791bdd8ef5b9bf325e075790bc
Jason Chow <jasonchow@meta.com>
2025-02-04 13:35:34 -0800

    update extension manager to use default mixin config finder (#613)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/aepsych/pull/613
    
    Make extension manage init signature match what would be found in config files.
    
    Reviewed By: crasanders
    
    Differential Revision: D69092287
    
    fbshipit-source-id: b2560be468d84551c0ef5b349cdc4a5d1cc5c77a

--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -5,6 +5,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+
 import unittest
 from pathlib import Path
 from types import ModuleType
@@ -16,7 +17,7 @@ class ExtensionsTest(unittest.TestCase):
     extension_path = Path(__file__).parent.parent / "extensions_example"
 
     def test_load_extensions(self):
-        exts = ExtensionManager(files=[self.extension_path / "new_objects.py"])
+        exts = ExtensionManager(extensions=[self.extension_path / "new_objects.py"])
         exts.load()
 
         self.assertTrue(len(exts.loaded_modules) == 1)
@@ -27,7 +28,7 @@ class ExtensionsTest(unittest.TestCase):
 
     def test_raise_missing_extension(self):
         exts = ExtensionManager(
-            files=[
+            extensions=[
                 self.extension_path / "new_objects.py",
                 self.extension_path / "missing_extension.py",
             ]
@@ -36,7 +37,7 @@ class ExtensionsTest(unittest.TestCase):
             exts.load()
 
     def test_double_load(self):
-        exts = ExtensionManager(files=[self.extension_path / "new_objects.py"])
+        exts = ExtensionManager(extensions=[self.extension_path / "new_objects.py"])
         exts.load()
 
         with self.assertWarns(UserWarning):
