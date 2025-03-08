fb5a030d9ba3950e64e89de91533c4cad7eed27b
Jason Chow <jasonchow@meta.com>
2025-03-08 12:28:05 -0800

    implement AffinePosteriorTransform (#651)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/aepsych/pull/651
    
    This is a thin wrapper around Botorch's ScalarizedPosteriorTransform that allows one to combine multiple outcomes into a single weighted outcome for optimization.
    
    Reviewed By: tymmsc
    
    Differential Revision: D69137323
    
    fbshipit-source-id: 9316e01a9ab5194dfe79ee95fcfefa1702ec4518

---
+++ aepsych/acquisition/objective/multi_outcome.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+from typing import Any, Dict, Optional
+
+import torch
+from aepsych.config import Config, ConfigurableMixin
+from botorch.acquisition.objective import ScalarizedPosteriorTransform
+
+
+class AffinePosteriorTransform(ScalarizedPosteriorTransform, ConfigurableMixin):
+    @classmethod
+    def get_config_options(
+        cls,
+        config: Config,
+        name: Optional[str] = None,
+        options: Optional[Dict[str, Any]] = None,
+    ) -> Dict[str, Any]:
+        """
+        Return a dictionary of the relevant options to initialize this class from the
+        config, even if it is outside of the named section. By default, this will look
+        for options in name based on the __init__'s arguments/defaults.
+
+        Args:
+            config (Config): Config to look for options in.
+            name (str, optional): Primary section to look for options for this class and
+                the name to infer options from other sections in the config.
+            options (Dict[str, Any], optional): Options to override from the config,
+                defaults to None.
+
+
+        Return:
+            Dict[str, Any]: A dictionary of options to initialize this class.
+        """
+        name = name or cls.__name__
+        options = super().get_config_options(config, name, options)
+
+        if "weights" not in options:
+            outcomes = config.getlist("common", "outcome_types", element_type=str)
+            options["weights"] = torch.ones(len(outcomes)) / len(outcomes)
+
+        return options
