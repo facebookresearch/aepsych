9274edcccee60e91fac17217822c39118fa4863c
Jason Chow <jasonchow@meta.com>
2025-03-21 18:21:33 -0700

    Add support for mixed likelihood constraints (#682)
    Summary:
    
    Added to GPClassification to allow mixed likelihood constraints.
    
    Differential Revision: D71429427

---
+++ aepsych/likelihoods/mixed.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch
+from aepsych.config import ConfigurableMixin
+from gpytorch.distributions import MultivariateNormal
+from gpytorch.likelihoods import Likelihood
+from gpytorch.mlls import VariationalELBO
+from gpytorch.mlls._approximate_mll import _ApproximateMarginalLogLikelihood
+from gpytorch.models import ApproximateGP
+from linear_operator.operators import DiagLinearOperator
+
+
+class ListLikelihood(Likelihood, ConfigurableMixin):
+    def __init__(self, likelihoods: list[Likelihood]) -> None:
+        """A wrapper around a list of likelihoods. Currently, the first
+        likelihood is given special treatment and any methods/functions that
+        would normally be called on the likelihood are called on the first. The
+        only special thing this class does is allow a list of likelihood to be
+        iterated over and indexed.
+
+        Args:
+            likelihoods (list[Likelihood]): A list of likelihoods where the
+                first is the primary one.
+
+        """
+        super().__init__()
+        self.likelihoods = torch.nn.ModuleList(likelihoods)
+
+    # def __getattr__(self, name):
+    #     if name == "likelihoods":
+    #         return self.likelihoods
+    #     else:
+    #         return getattr(self.likelihoods[0], name)
+
+    def __iter__(self):
+        return iter(self.likelihoods)
+
+    def __getitem__(self, key):
+        return self.likelihoods[key]
+
+    def forward(self, *args, **kwargs):
+        """Forward pass for the likelihood. This is a no-op for this class.
+        When a forward is called on a class instance, the forward of the first
+        likelihood is called instead."""
+        return self.likelihoods[0].forward(*args, **kwargs)
+
+
+class MixedVariationalELBO(_ApproximateMarginalLogLikelihood):
+    def __init__(
+        self,
+        likelihood: ListLikelihood,
+        task_indices: list[torch.Tensor],
+        model: ApproximateGP,
+        num_data: int,
+        beta: float = 1.0,
+        combine_terms: bool = True,
+    ) -> None:
+        """A variant of the variational ELBO that takes multiple likelihoods.
+        Only the first likelihood is learnable. Used for adding constraints to
+        variational models.
+
+        Args:
+        likelihood (ListLikelihood): An instance of the ListLikelihood class
+            that represents a list of likelihoods. Only the first likelihood is
+            learnable.
+            All likelihoods have to be univariate one-dimensional likelihood.
+        task_indices (list[torch.Tensor]): A list of tensors of indices that
+            correspond to each task/data type being modeled, one for each
+            likelihood.
+        model (gpytorch.models.ApproximateGP): The approximate GP model
+        num_data (int): The total number of training data points (necessary for
+            SGD)
+        beta (float): A multiplicative factor for the KL divergence term.
+            Setting it to 1 (default) recovers true variational inference.
+            Setting it to anything less than 1 reduces the regularization effect
+            of the model.
+        combine_terms (bool): Whether or not to sum the expected NLL with the
+            KL terms (default True)
+        """
+        super().__init__(
+            likelihood=likelihood[0],
+            model=model,
+            num_data=num_data,
+            beta=beta,
+            combine_terms=combine_terms,
+        )
+
+        self.list_likelihoods = likelihood
+        self.task_indices = task_indices
+
+    def _log_likelihood_term(
+        self, variational_dist_f: MultivariateNormal, target: torch.Tensor, **kwargs
+    ) -> torch.Tensor:
+        mean = variational_dist_f.loc
+        variance = variational_dist_f.variance
+
+        """
+        Linear Operator is not very efficient in indexing.
+        For instance, covar[indices.unsqueeze(-1), indices.unsqueeze(-2)]
+        consumes an unnecessary amount of memory.
+        Therefore, we opt in for the following hacks.
+        """
+        res = torch.tensor(0.0).to(mean)
+        for likelihood, indices in zip(self.list_likelihoods, self.task_indices):
+            res = res + likelihood.expected_log_prob(
+                target[indices],
+                MultivariateNormal(
+                    mean[indices],
+                    DiagLinearOperator(diag=variance[indices]),
+                ),
+                **kwargs,
+            ).sum(-1)
+
+        return res
+
+    def forward(
+        self, variational_dist_f: MultivariateNormal, target: torch.Tensor, **kwargs
+    ) -> torch.Tensor:
+        """
+        Computes the Variational ELBO given the variational distribution and
+        the target data. Calling this function will call this class's
+        log_likelihood_term, which will sum over the expected log probabilities
+        of each likelihood and its corresponding task.
+
+        Args:
+        variational_dist_f (gpytorch.distributions.MultivariateNormal): The
+            variational distribution of the latent function.
+        target (Tensor): The target data.
+        **kwargs: Additional arguments passed to the likelihood's
+            expected_log_prob. These kwargs are passed to every likelihood in
+            ListLikelihood.
+
+        Returns:
+            torch.Tensor: The variational ELBO. Output shape corresponds to
+                batch shape of the model/input data.
+        """
+        return super().forward(variational_dist_f, target, **kwargs)
--- aepsych/models/gp_classification.py
+++ aepsych/models/gp_classification.py
@@ -6,14 +6,21 @@
 # LICENSE file in the root directory of this source tree.
 from __future__ import annotations
 
+import warnings
 from typing import Any, Dict, Optional, Tuple
 
 import gpytorch
 import torch
+from aepsych.likelihoods.mixed import ListLikelihood, MixedVariationalELBO
+from aepsych.models.inducing_points import FixedPlusAllocator, GreedyVarianceReduction
 from aepsych.models.inducing_points.base import InducingPointAllocator
 from aepsych.utils_logging import getLogger
 from botorch.posteriors import TransformedPosterior
-from gpytorch.likelihoods import BernoulliLikelihood, Likelihood
+from gpytorch.likelihoods import (
+    BernoulliLikelihood,
+    FixedNoiseGaussianLikelihood,
+    Likelihood,
+)
 
 from .transformed_posteriors import BernoulliProbitProbabilityPosterior
 from .variational_gp import VariationalGPModel
@@ -46,6 +53,9 @@ class GPClassificationModel(VariationalGPModel):
         mll_class: Optional[gpytorch.mlls.MarginalLogLikelihood] = None,
         inducing_point_method: Optional[InducingPointAllocator] = None,
         inducing_size: int = 100,
+        constraint_locations: Optional[torch.Tensor] = None,
+        constraint_values: Optional[torch.Tensor] = None,
+        constraint_strengths: Optional[torch.Tensor] = None,
         max_fit_time: Optional[float] = None,
         optimizer_options: Optional[Dict[str, Any]] = None,
     ) -> None:
@@ -63,13 +73,84 @@ class GPClassificationModel(VariationalGPModel):
             inducing_point_method (InducingPointAllocator, optional): The method to use for selecting inducing points.
                 If not set, a GreedyVarianceReduction is made.
             inducing_size (int): Number of inducing points. Defaults to 100.
+            constraint_locations (torch.Tensor, optional): Locations at which to constrain the latent function. Defaults
+                to None.
+            constraint_values (torch.Tensor, optional): Values at which to constrain the latent function. There must be
+                one value for each constraint location. Assumed to be in probability space unless likelihood is not None,
+                If likelihood is set, no transformations to these values will be done. Defaults to None.
+            constraint_strengths (torch.Tensor, optional): Strength of the constraints. There must be one value for each
+                constraint location. If set to None, constraint strength will be based on the value (0.2 * value + 0.1).
             max_fit_time (float, optional): The maximum amount of time, in seconds, to spend fitting the model. If None,
                 there is no limit to the fitting time.
             optimizer_options (Dict[str, Any], optional): Optimizer options to pass to the SciPy optimizer during
                 fitting. Assumes we are using L-BFGS-B.
         """
+        self.constraint_locations = constraint_locations
+        self.constraint_values = constraint_values
+        self.constraint_strengths = constraint_strengths
+
         if likelihood is None:
             likelihood = BernoulliLikelihood()
+
+            if self.constraint_values is not None:
+                self.constraint_values = torch.distributions.Normal(0, 1).icdf(
+                    self.constraint_values
+                )
+
+        if self.constraint_locations is not None and mll_class is not None:
+            raise ValueError(
+                "Cannot set both constraints and mll_class. "
+                "To use constraints, we create a mixed variational ELBO for the MLL."
+            )
+
+        # Check if constraints are all the same size
+        if self.constraint_locations is not None and self.constraint_values is not None:
+            # Constraints need to be 2D
+            if self.constraint_locations.dim() != 2:
+                raise ValueError("Constraint locations must be shaped (n, d).")
+
+            if self.constraint_values.dim() != 2:
+                raise ValueError("Constraint values must be (n, 1).")
+
+            # Needs the same n
+            if self.constraint_locations.shape[0] != self.constraint_values.shape[0]:
+                raise ValueError("Constraint locations and values must have the same n")
+
+            # Manual strengths need to match values
+            if self.constraint_strengths is not None:
+                if (
+                    self.constraint_values.shape[0]
+                    != self.constraint_strengths.shape[0]
+                ):
+                    raise ValueError(
+                        "Constraint locations, values, and strengths must have the same n"
+                    )
+            else:
+                # Make strengths based on values
+                self.constraint_strengths = (0.2 * self.constraint_values + 0.1) ** 2
+
+            # Replace necessary objects
+            mll_class = MixedVariationalELBO
+            likelihood = ListLikelihood(
+                likelihoods=[
+                    likelihood,
+                    FixedNoiseGaussianLikelihood(
+                        noise=self.constraint_strengths.squeeze()
+                    ),
+                ],
+            )
+            if inducing_point_method is None:
+                inducing_point_method = FixedPlusAllocator(
+                    dim=dim,
+                    points=self.constraint_locations,
+                    main_allocator=GreedyVarianceReduction,
+                )
+            else:
+                inducing_point_method = FixedPlusAllocator(
+                    dim=dim,
+                    points=self.constraint_locations,
+                    main_allocator=inducing_point_method,
+                )
 
         super().__init__(
             dim=dim,
@@ -82,6 +163,87 @@ class GPClassificationModel(VariationalGPModel):
             max_fit_time=max_fit_time,
             optimizer_options=optimizer_options,
         )
+
+    def set_train_data(
+        self,
+        inputs: Optional[torch.Tensor] = None,
+        targets: Optional[torch.Tensor] = None,
+        strict: bool = True,
+    ) -> None:
+        """Set the training data for the model.
+
+        Args:
+            inputs (torch.Tensor, optional): The new training inputs X.
+            targets (torch.Tensor, optional): The new training targets Y.
+            strict (bool): Whether to strictly enforce the device of the inputs and targets.
+        """
+        if not strict:
+            warnings.warn(
+                "strict set to false, but set_train_data will always follow device",
+                stacklevel=2,
+            )
+
+        if self.constraint_locations is not None and self.constraint_values is not None:
+            if inputs is not None:
+                inputs = torch.cat((inputs, self.constraint_locations), dim=0)
+            if targets is not None:
+                targets = torch.cat((targets, self.constraint_values), dim=0)
+
+        if inputs is None:
+            self.train_inputs = None
+        else:
+            self.train_inputs = (inputs,)
+        self.train_targets = targets
+
+    def fit(
+        self,
+        train_x: torch.Tensor,
+        train_y: torch.Tensor,
+        warmstart_hyperparams: bool = False,
+        warmstart_induc: bool = False,
+        **kwargs,
+    ) -> None:
+        """Fit underlying model.
+
+        Args:
+            train_x (torch.Tensor): Inputs.
+            train_y (torch.LongTensor): Responses.
+            warmstart_hyperparams (bool): Whether to reuse the previous hyperparameters (True) or fit from scratch
+                (False). Defaults to False.
+            warmstart_induc (bool): Whether to reuse the previous inducing points or fit from scratch (False).
+                Defaults to False.
+        """
+        self.set_train_data(train_x, train_y)
+
+        # by default we reuse the model state and likelihood. If we
+        # want a fresh fit (no warm start), copy the state from class initialization.
+        if not warmstart_hyperparams:
+            self._reset_hyperparameters()
+
+        if not warmstart_induc or (
+            self.inducing_point_method.last_allocator_used is None
+        ):
+            self._reset_variational_strategy()
+
+        n = train_y.shape[0]
+
+        if self.constraint_locations is not None and self.constraint_values is not None:
+            mll = MixedVariationalELBO(
+                likelihood=self.likelihood,
+                task_indices=[
+                    torch.arange(n),
+                    torch.arange(n, n + self.constraint_values.shape[0]),
+                ],
+                model=self,
+                num_data=n,
+            )
+        else:
+            mll = self.mll_class(self.likelihood, self, n)
+
+        if "optimizer_kwargs" in kwargs:
+            self._fit_mll(mll, **kwargs)
+        else:
+            self._fit_mll(mll, optimizer_kwargs=self.optimizer_options, **kwargs)
 
     def predict(
         self, x: torch.Tensor, probability_space: bool = False
