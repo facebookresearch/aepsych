f90e5fde504b063ccabd39e10a2ae7c94e54d0bb
Jason Chow <jasonchow@meta.com>
2025-03-27 11:07:18 -0700

    Rewrite dim_grid to just use simple torch functions (#703)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/aepsych/pull/703
    
    dim_grid used to use arcane numpy techniques, cleaned this up to only use pytorch functions.
    
    Reviewed By: adellari
    
    Differential Revision: D71942737
    
    fbshipit-source-id: 3fb1504e9f6cd380c4cb5dba76966521dff47c07

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -46,6 +46,27 @@ class UtilsTestCase(unittest.TestCase):
 
         self.assertTrue(np.all(grid.shape == (900, 3)))
 
+    def test_dim_grid_values(self):
+        """Test that the grid points are correctly spaced."""
+        lb = torch.tensor([0.0, 1.0])
+        ub = torch.tensor([1.0, 3.0])
+        gridsize = 5
+
+        grid = dim_grid(lb, ub, gridsize=gridsize)
+
+        # Check shape
+        self.assertEqual(grid.shape, (25, 2))
+
+        # Check values for first dimension
+        expected_x_values = torch.tensor([0.0, 0.25, 0.5, 0.75, 1.0])
+        x_values = torch.unique(grid[:, 0])
+        self.assertTrue(torch.allclose(x_values, expected_x_values))
+
+        # Check values for second dimension
+        expected_y_values = torch.tensor([1.0, 1.5, 2.0, 2.5, 3.0])
+        y_values = torch.unique(grid[:, 1])
+        self.assertTrue(torch.allclose(y_values, expected_y_values))
+
     def test_process_bounds(self):
         lb, ub, dim = _process_bounds(np.r_[0, 1], np.r_[2, 3], None)
         self.assertTrue(torch.all(lb == torch.tensor([0.0, 1.0])))
