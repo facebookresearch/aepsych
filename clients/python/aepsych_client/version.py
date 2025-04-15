7f329db3e752807b2fa06f625c372f0d66e26101
Jason Chow <jasonchow@meta.com>
2025-04-15 10:54:13 -0700

    Automate build and release for python client
    Summary: Python client will automatically build and release like the main package when the release-client branch is pushed to.
    
    Differential Revision: D73049039

---
+++ clients/python/aepsych_client/version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+__version__ = "0.4.1+dev"
