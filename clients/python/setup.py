cc8067c004e309088d1445a2b3db390637700fb9
Jason K. Chow <jasonchow@meta.com>
2025-04-16 11:06:22 -0700

    Fix automated client release process (#780)
    Summary:
    Version wasn't being updated properly and the artifact name was mismatched. Fixing these gets a release out.
    
    Pull Request resolved: https://github.com/facebookresearch/aepsych/pull/780
    
    Test Plan: a client release was successful
    
    Reviewed By: adellari
    
    Differential Revision: D73122481
    
    Pulled By: JasonKChow
    
    fbshipit-source-id: d6354805507bf71fdfc0d6a739d38703975438de

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+import os
 from os import path
 
 from setuptools import find_packages, setup
@@ -12,9 +13,14 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "Readme.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+with open(os.path.join("aepsych_client", "version.py"), "r") as fh:
+    for line in fh.readlines():
+        if line.startswith("__version__"):
+            version = line.split('"')[1]
+
 setup(
     name="aepsych_client",
-    version="0.3.0",
+    version=version,
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
