556a5a170a00864e61cfc27bce85ecc4df810849
Jason K. Chow <jasonchow@meta.com>
2025-04-16 16:58:07 +0000

    include Readme and version in build

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -12,9 +12,14 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "Readme.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+with open(os.path.join("aepsych_client", "version.py"), "r") as fh:
+    for line in fh.readlines():
+        if line.startswith("__version__"):
+            version = line.split('"')[1]
+
 setup(
     name="aepsych_client",
-    version="0.3.0",
+    version=version,
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
