84fa231ecdca5b906fff5bbab4d9ac432709f35e
Jason Chow <jasonchow@fb.com>
2025-05-05 11:51:25 -0700

    Temporary attribute context manager for factories
    Summary: Utility context manager that allows us to temporarily modify the class attributes of a factory to make a module for a subclass. Now used in the pairwise factory.
    
    Differential Revision: D74192292

--- aepsych/factory/utils.py
+++ aepsych/factory/utils.py
@@ -4,9 +4,24 @@
 
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-
+from contextlib import contextmanager
 
 __default_invgamma_concentration = 4.6
 __default_invgamma_rate = 1.0
 DEFAULT_INVGAMMA_CONC = 4.6
 DEFAULT_INVGAMMA_RATE = 1.0
+
+
+@contextmanager
+def temporary_attributes(obj, **kwargs):
+    """Temporarily sets attributes on an object, and restores them when the context exits."""
+
+    try:
+        old_attrs = {}
+        for attr, val in kwargs.items():
+            old_attrs[attr] = getattr(obj, attr)
+            setattr(obj, attr, val)
+        yield obj
+    finally:
+        for attr, val in old_attrs.items():
+            setattr(obj, attr, val)
